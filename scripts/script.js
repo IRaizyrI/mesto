a92333e0cd258278fe6966ae83559f73e125763e
Raizyr <raizyr@raizyr.ru>
2022-08-22 11:04:47 +0300

    Initial

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+let popupTitle = document.querySelector('.popup__element_title');
+let popupSubtitle = document.querySelector('.popup__element_subtitle');
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileText = document.querySelector('.profile__text');
+
+function openPopup() {
+    popupTitle.value = profileName.textContent;
+    popupSubtitle.value = profileText.textContent;
+    popup.classList.add('popup_opened');
+}
+function toggleLike(evt) {
+    evt.target.classList.toggle('element__like_active'); 
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function submitForm(evt) {
+    evt.preventDefault();
+    profileName.textContent = popupTitle.value;
+    profileText.textContent = popupSubtitle.value;
+    closePopup();
+}
+
+document.querySelector('.profile__edit').addEventListener('click', openPopup);
+document.querySelector('.popup__close').addEventListener('click', closePopup);
+document.querySelector('.popup__container').addEventListener('submit', submitForm);
+document.querySelectorAll('.element__like').forEach(item => {
+    item.addEventListener('click', toggleLike);
+});
