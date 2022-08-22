f0cacd5e4a5fd8c527c12abda1e48a68f3001699
Raizyr <raizyr@raizyr.ru>
2022-08-22 16:24:20 +0300

    removed free space

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,32 @@
-let popupTitle = document.querySelector('.popup__element_title');
-let popupSubtitle = document.querySelector('.popup__element_subtitle');
+let popupTitle = document.querySelector('.popup__input-title');
+let popupSubtitle = document.querySelector('.popup__input-subtitle');
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
 
 function openPopup() {
-    popupTitle.value = profileName.textContent;
-    popupSubtitle.value = profileText.textContent;
-    popup.classList.add('popup_opened');
+  popupTitle.value = profileName.textContent;
+  popupSubtitle.value = profileText.textContent;
+  popup.classList.add('popup_is-opened');
 }
 function toggleLike(evt) {
-    evt.target.classList.toggle('element__like_active'); 
+  evt.target.classList.toggle('element__like_active');
 }
 
 function closePopup() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_is-opened');
 }
 
 function submitForm(evt) {
-    evt.preventDefault();
-    profileName.textContent = popupTitle.value;
-    profileText.textContent = popupSubtitle.value;
-    closePopup();
+  evt.preventDefault();
+  profileName.textContent = popupTitle.value;
+  profileText.textContent = popupSubtitle.value;
+  closePopup();
 }
 
 document.querySelector('.profile__edit').addEventListener('click', openPopup);
 document.querySelector('.popup__close').addEventListener('click', closePopup);
 document.querySelector('.popup__container').addEventListener('submit', submitForm);
 document.querySelectorAll('.element__like').forEach(item => {
-    item.addEventListener('click', toggleLike);
+  item.addEventListener('click', toggleLike);
 });
